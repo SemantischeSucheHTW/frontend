568ef87dda972b8a2524e181b1e1904ffd8bb6b0
JPOstholt <jascha.ostholt@gmail.com>
2018-12-21 14:30:34 +0100

    initialized vue app

---
+++ src/components/Default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Willkommen</h1>
+    </div>
+</template>
+
+<script>
+    import http from './../core/http';
+
+    export default {
+        name: 'Default'
+    }
+</script>
+
+<style scoped="true">
+    .container {
+        margin-top: 30px;
+    }
+</style>
---
+++ src/core/http.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import axios from 'axios';
+
+export default axios;
