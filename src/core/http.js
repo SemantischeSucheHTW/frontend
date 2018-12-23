88663f4657ed03f0118abc3c4ec855b869eb97f6
JPOstholt <jascha.ostholt@gmail.com>
2018-12-23 16:35:38 +0100

    added interceptor with base url to http config

--- src/core/http.js
+++ src/core/http.js
@@ -2,4 +2,10 @@
 
 import axios from 'axios';
 
+axios.interceptors.request.use(config => {
+    const baseUrl = "http://localhost:8108";
+    config.baseURL = baseUrl;
+    return config;
+});
+
 export default axios;
